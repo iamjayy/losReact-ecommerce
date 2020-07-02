4c351a363796e440ed3e51482043c4a3ce5d5c4a
jason <jason_doan94@yahoo.com>
2020-07-01 21:50:07 -0400

    Updates

---
+++ precache-manifest.91742a64e99e073d6b8128d6404734ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56e442e293219f186e0d3f3506bfb56b",
+    "url": "/losReact-ecommerce/index.html"
+  },
+  {
+    "revision": "bb4e7906b09ec63350f7",
+    "url": "/losReact-ecommerce/static/css/main.0d7eef26.chunk.css"
+  },
+  {
+    "revision": "8dbcfffaaf1e311a4623",
+    "url": "/losReact-ecommerce/static/js/2.d789395e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/losReact-ecommerce/static/js/2.d789395e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb4e7906b09ec63350f7",
+    "url": "/losReact-ecommerce/static/js/main.0ada8b2a.chunk.js"
+  },
+  {
+    "revision": "cf6e4f55c90432f47974",
+    "url": "/losReact-ecommerce/static/js/runtime-main.4a886ef7.js"
+  },
+  {
+    "revision": "395080bec9e0bd056c6996edc5d4b8ed",
+    "url": "/losReact-ecommerce/static/media/city.395080be.jpeg"
+  }
+]);
